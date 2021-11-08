14122eddf59994f87af459f38f19dcdefc0c8e52
dvvill <91037458+dvvill@users.noreply.github.com>
2021-11-08 04:35:17 -0500

    Third Party API

--- App.js
+++ App.js
@@ -1,31 +1,46 @@
-import React from "react"; 
+import React, { useState, useEffect } from "react"; 
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import CompletedList from "./CompletedList";
 import ToDoList from "./ToDoList";
-
-
+import GifList from './GifList';
+import axios from 'axios';
 
 
 function App() {
-   return (
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios(`http://api.giphy.com/v1/gifs/trending?&limit=3&api_key=EmU9dpXgxO9PChhSA8GxpHFFNVqwufK1`)
+        .then(response => setData(response.data.data))
+        .catch(error => console.log('error fetching and parsing data', error))
+        ;
+      }, []);
+
+  return (
     <div className="App">
         <h1>Todo App</h1>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="ToDoList" element={<ToDoList />} />
-        <Route path="CompletedList" element={<CompletedList/>}/>
+       <Route path="CompletedList" element={<CompletedList/>}/>
       </Routes>
+
+      <div className="gif-section">
+        <GifList data={data} />
+      </div>
 
         <nav className="nav-link">
                 <Link to="/ToDoList">To Do List</Link>
                 <Link to="/CompletedList">Completed List</Link>
         </nav>
+<nav>
+
+</nav>
+
 </div>
   );
   }
-  
   function Home() {
     return (
       <>
